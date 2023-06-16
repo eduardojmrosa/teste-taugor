54575e1e94f49ef6c3d755fb2f5c1982f2d53b5a
Eduardo José de Mello <eduardojose222002@hotmail.com>
2023-06-16 13:43:57 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,13 +14,7 @@ import { AuthProvider } from "./Auth";
 import { PrivateRoute } from "./routes/PrivateRoute";
 
 export const firebaseApp = initializeApp({
-  apiKey: "AIzaSyCexjTKI1v_4ASkmGBNrQ3hdlXPJmc8KTs",
-  authDomain: "velvety-striker-313719.firebaseapp.com",
-  projectId: "velvety-striker-313719",
-  storageBucket: "velvety-striker-313719.appspot.com",
-  messagingSenderId: "887606094911",
-  appId: "1:887606094911:web:1cb3a16a2dd810d42f1c8a",
-  measurementId: "G-THCM377XVB",
+ //Firebase credentials
 });
 
 export const auth = getAuth(firebaseApp);
